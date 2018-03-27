b3cc7d36cdd6dbbf70faaedee33680e8f5aa7629
carlogilmar <carlogilmar12@gmail.com>
2018-03-27 17:26:31 -0600

    Inicializando el scores channel en el front

--- assets/js/racer_controller.js
+++ assets/js/racer_controller.js
@@ -3,14 +3,12 @@ import socket from "./socket"
 export var RacerController = {
 
   initChannel: () =>{
-    console.log("init channel")
     var channel = socket.channel("timer:update", {})
       channel.join()
       .receive("ok", resp => { console.log("Timer Channel Joined 😉", resp) })
       .receive("error", resp => { console.log("No se puede conectar al Timer Channel", resp) })
 
       channel.on("new_time", msg => {
-        console.log("The timer is ", msg.time)
           document.getElementById('status').innerHTML = msg.response
           document.getElementById('timer').innerHTML = msg.time
 
@@ -28,9 +26,8 @@ export var RacerController = {
   },
 
   initChannelPlayers: () => {
-    console.log("Init players channel")
-    let channelPlayer = socket.channel("players", {uuid: Math.floor((Math.random() * 10000) + 1)})
 
+    let channelPlayer = socket.channel("players", {uuid: Math.floor((Math.random() * 10000) + 1)})
     channelPlayer.on("players_list", msg => {
     $("#list_users").html("")
      $.each(msg.users, function( index, value ) {
@@ -46,9 +43,22 @@ export var RacerController = {
     channelPlayer.push('get_list', {})
   },
 
+  initChannelScores: () =>{
+    var channel = socket.channel("scores", {})
+      channel.join()
+      .receive("ok", resp => { console.log("Scores Channel Joined 😙 ", resp) })
+      .receive("error", resp => { console.log("No se puede conectar al Scores Channel", resp) })
+
+			// Enviar user id y score
+      channel
+      .push('scores:set', {user:"carlogilmar", score:100})
+      .receive('ok', resp => { console.log("Starter timer",resp) })
+  },
+
   bindEvents:function (){
     this.initChannel()
     this.initChannelPlayers()
+    this.initChannelScores()
   },
 
   start: function(){
