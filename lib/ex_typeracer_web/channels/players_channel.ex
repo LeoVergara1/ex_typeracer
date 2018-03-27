563992a57cf628247b2d409a1b8dfd9cf2aff7d2
carlogilmar <carlogilmar12@gmail.com>
2018-03-27 15:12:14 -0600

    Ajustando el channel

--- lib/ex_typeracer_web/channels/players_channel.ex
+++ lib/ex_typeracer_web/channels/players_channel.ex
@@ -4,14 +4,14 @@ defmodule ExTyperacerWeb.PlayersChannel do
   def join("players", payload, socket) do
     IO.inspect payload
     [{_, users_list}] = :ets.lookup(:mapShared, "users")
-    users_list = List.insert_at(users_list, length(users_list)+1, payload)
+    users_list = List.insert_at(users_list, length(users_list), payload)
     :ets.insert(:mapShared, {"users", users_list})
     {:ok, socket}
   end
 
   def handle_in("get_list", payload, socket) do
-    IO.puts "Aqui ando"
-    broadcast! socket, "players_list", %{"pass:" => "algo"}
+    [{_, users_list}] = :ets.lookup(:mapShared, "users")
+    broadcast! socket, "players_list", %{"users:" => users_list}
     {:noreply, socket}
   end
 
