9a45f1d729c0266b0f57addc1631df4529670ef6
carlogilmar <carlogilmar12@gmail.com>
2018-03-27 14:38:25 -0600

    Comentando codigo que ya no se usa

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -51,26 +51,26 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 // Finally, pass the token on connect as below. Or remove it
 // from connect if you don't care about authentication.
 
-let channel = socket.channel("lobby", { uuid: Math.floor((Math.random() * 10000) + 1) });
-let list    = $('#message-list');
-let message = $('#message');
-let name    = $('#name');
-
-message.on('keypress', event => {
-  if (event.keyCode == 13) {
-    channel.push('new_message', { name: name.val(), message: message.val() });
-    message.val('');
-  }
-});
-
-channel.on('new_message', payload => {
-  list.append(`<b>${payload.name || 'Anonymous'}:</b> ${payload.message}<br>`);
-  list.prop({scrollTop: list.prop("scrollHeight")});
-});
-
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+//let channel = socket.channel("lobby", { uuid: Math.floor((Math.random() * 10000) + 1) });
+//let list    = $('#message-list');
+//let message = $('#message');
+//let name    = $('#name');
+//
+//message.on('keypress', event => {
+//  if (event.keyCode == 13) {
+//    channel.push('new_message', { name: name.val(), message: message.val() });
+//    message.val('');
+//  }
+//});
+//
+//channel.on('new_message', payload => {
+//  list.append(`<b>${payload.name || 'Anonymous'}:</b> ${payload.message}<br>`);
+//  list.prop({scrollTop: list.prop("scrollHeight")});
+//});
+//
+//channel.join()
+//  .receive("ok", resp => { console.log("Joined successfully", resp) })
+//  .receive("error", resp => { console.log("Unable to join", resp) })
 
 socket.connect()
 
