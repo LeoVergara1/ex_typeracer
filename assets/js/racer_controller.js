44ae333a78dda81646a76dce34f1429166bbea6c
carlogilmar <carlogilmar12@gmail.com>
2018-03-27 12:31:06 -0600

    Agregando race controller js

---
+++ assets/js/racer_controller.js
@@ -0,0 +1,34 @@
+import socket from "./socket"
+
+export var RacerController = {
+
+  initChannel: () =>{
+    console.log("init channel")
+    var channel = socket.channel("timer:update", {})
+      channel.join()
+      .receive("ok", resp => { console.log("Timer Channel Joined 😉", resp) })
+      .receive("error", resp => { console.log("No se puede conectar al Timer Channel", resp) })
+
+      channel.on("new_time", msg => {
+        console.log("The timer is ", msg.time)
+          document.getElementById('status').innerHTML = msg.response
+          document.getElementById('timer').innerHTML = msg.time
+      });
+
+    $("#start-timer").on("click" , () =>{
+      channel
+      .push('start_timer', {})
+      .receive('ok', resp => { console.log("Starter timer",resp) })
+    })
+  },
+
+  bindEvents:function (){
+    this.initChannel()
+  },
+
+  start: function(){
+    this.bindEvents()
+  }
+
+}
+
