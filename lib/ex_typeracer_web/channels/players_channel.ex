6013e3c81ebf7af2187091e9117495df19d4b8c0
carlogilmar <carlogilmar12@gmail.com>
2018-03-28 13:35:26 -0600

    Retirando mexican debuggers y poniendo logs chidos 😍

--- lib/ex_typeracer_web/channels/players_channel.ex
+++ lib/ex_typeracer_web/channels/players_channel.ex
@@ -1,10 +1,12 @@
 defmodule ExTyperacerWeb.PlayersChannel do
   use Phoenix.Channel
+  require Logger
 
   def join("players", payload, socket) do
     [{_, map}] = :ets.lookup(:mapShared, "users")
     usersUpdated = Map.put_new(map, socket.id, String.to_atom(Integer.to_string(payload["uuid"])))
     :ets.insert(:mapShared, {"users", usersUpdated})
+    Logger.info ":: Players:channel:: Conexión de un usuario"
     {:ok, socket}
   end
 
@@ -17,6 +19,7 @@ defmodule ExTyperacerWeb.PlayersChannel do
   def terminate(reason, socket) do
     [{_, map}] = :ets.lookup(:mapShared, "users")
     mapUpdated = Map.delete(map, socket.id)
+    Logger.info ":: Players:channel:: Desconexión de un usuario. Actualizando usuarios activos"
     :ets.insert(:mapShared, {"users", mapUpdated})
     broadcast! socket, "players_list", %{"users" => mapUpdated}
     :ok
--- lib/ex_typeracer_web/channels/timer_channel.ex
+++ lib/ex_typeracer_web/channels/timer_channel.ex
@@ -3,18 +3,15 @@ defmodule ExTyperacerWeb.TimerChannel do
   use Phoenix.Channel
 
   def join("timer:update", _msg, socket) do
-    IO.puts(" Alguien se conecto al timer channel!!!!!! ")
     {:ok, socket}
   end
 
   def handle_in("new_time", msg, socket) do
     push socket, "new_time", msg
-		IO.puts "=======> =====> ========> New Timer Handle!!"
     {:noreply, socket}
   end
 
 	def handle_in("start_timer", _, socket) do
-		IO.puts "=======> =====> ========> Start Timer!!"
 		ExTyperacerWeb.Endpoint.broadcast("timer:start", "start_timer", %{})
 		{:noreply, socket}
 	end
