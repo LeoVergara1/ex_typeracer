12e7e8bb1f3498ca08eb56522f54713c554aa03b
carlogilmar <carlogilmar12@gmail.com>
2018-03-28 13:31:21 -0600

    Agregando terminate function

--- lib/ex_typeracer_web/channels/players_channel.ex
+++ lib/ex_typeracer_web/channels/players_channel.ex
@@ -2,17 +2,24 @@ defmodule ExTyperacerWeb.PlayersChannel do
   use Phoenix.Channel
 
   def join("players", payload, socket) do
-    IO.inspect payload
-    [{_, users_list}] = :ets.lookup(:mapShared, "users")
-    users_list = List.insert_at(users_list, length(users_list), payload)
-    :ets.insert(:mapShared, {"users", users_list})
+    [{_, map}] = :ets.lookup(:mapShared, "users")
+    usersUpdated = Map.put_new(map, socket.id, String.to_atom(Integer.to_string(payload["uuid"])))
+    :ets.insert(:mapShared, {"users", usersUpdated})
     {:ok, socket}
   end
 
   def handle_in("get_list", payload, socket) do
-    [{_, users_list}] = :ets.lookup(:mapShared, "users")
-    broadcast! socket, "players_list", %{"users" => users_list}
+    [{_, map}] = :ets.lookup(:mapShared, "users")
+    broadcast! socket, "players_list", %{"users" => map}
     {:noreply, socket}
+  end
+
+  def terminate(reason, socket) do
+    [{_, map}] = :ets.lookup(:mapShared, "users")
+    mapUpdated = Map.delete(map, socket.id)
+    :ets.insert(:mapShared, {"users", mapUpdated})
+    broadcast! socket, "players_list", %{"users" => mapUpdated}
+    :ok
   end
 
 end
