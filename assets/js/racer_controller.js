6307fb5b848f2369662bac1986044bf4435d90a5
carlogilmar <carlogilmar12@gmail.com>
2018-03-28 09:10:20 -0600

    Agregando canal en frontend para recibir los scores

--- assets/js/racer_controller.js
+++ assets/js/racer_controller.js
@@ -28,31 +28,41 @@ export var RacerController = {
   initChannelPlayers: () => {
 
     let channelPlayer = socket.channel("players", {uuid: Math.floor((Math.random() * 10000) + 1)})
-    channelPlayer.on("players_list", msg => {
-    $("#list_users").html("")
-     $.each(msg.users, function( index, value ) {
-       $("#list_users").append(`<p>username: ${value.uuid}</p>`)
-     });
-   });
 
-    channelPlayer.join()
-      .receive("ok", resp => {
-        console.log("Player successfully 😎", resp)
-      })
-      .receive("error", resp => { console.log("Unable to join", resp) })
-    channelPlayer.push('get_list', {})
+			channelPlayer.on("players_list", msg => {
+				$("#list_users").html("")
+					$.each(msg.users, function( index, value ) {
+						$("#list_users").append(`<p>username: ${value.uuid}</p>`)
+					});
+			});
+
+			channelPlayer.join()
+				.receive("ok", resp => {
+					console.log("Player successfully 😎", resp)
+				})
+				.receive("error", resp => { console.log("Unable to join", resp) })
+
+			channelPlayer.push('get_list', {})
   },
 
   initChannelScores: () =>{
     var channel = socket.channel("scores", {})
+			//Join al Channel de Scores
       channel.join()
       .receive("ok", resp => { console.log("Scores Channel Joined 😙 ", resp) })
       .receive("error", resp => { console.log("No se puede conectar al Scores Channel", resp) })
-
-			// Enviar user id y score
+			// Socket para registrar los scores por usuario
       channel
       .push('scores:set', {user:"carlogilmar", score:100})
       .receive('ok', resp => { console.log("Starter timer",resp) })
+			// Socket para traer todos los scores
+      channel
+      .push('scores:get', { user: Math.floor((Math.random() * 10000) + 1) })
+      .receive('ok', resp => { console.log("ok",resp) })
+			// Socket donde llegarán todos los scores
+			channel.on("scores:show", msg => {
+				console.log(msg)
+			});
   },
 
   bindEvents:function (){
