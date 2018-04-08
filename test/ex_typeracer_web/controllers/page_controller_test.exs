9fdf28a45b6023d1ca1d8745edac9bea38cba4cf
neodevelop <neodevelop@gmail.com>
2018-04-07 22:47:08 -0500

    #16 Fixing the test for PageController

--- test/ex_typeracer_web/controllers/page_controller_test.exs
+++ test/ex_typeracer_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ExTyperacerWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to the chat"
   end
 end
