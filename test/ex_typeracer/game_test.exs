f792bcf6c7c46ef9431f60752a6a1f723b66845a
neodevelop <neodevelop@gmail.com>
2018-04-08 18:55:42 -0500

    #16 Refactor the test

--- test/ex_typeracer/game_test.exs
+++ test/ex_typeracer/game_test.exs
@@ -15,21 +15,24 @@ defmodule ExTyperacer.Structs.GameTest do
 
   test "add a new player" do
     game = Game.new()
-    updated_game = Game.add_player(game, "neodevelop")
-    assert Enum.count(updated_game.players) == 1
+    game = Game.add_player(game, "neodevelop")
+    assert Enum.count(game.players) == 1
   end
 
   test "add the same player twice" do
   end
 
   test "playing the game" do
-    game = %{ Game.new() | paragraph: "Hello Typeracer, we're MakingDevs." }
-    updated_game = Game.add_player(game, "neodevelop")
-    assert updated_game.paragraph == "Hello Typeracer, we're MakingDevs."
-    updated_game = Game.plays updated_game, "neodevelop", "Hello"
-    player = Enum.find(updated_game.players, fn %Player{username: "neodevelop"} -> :ok end)
+    game = %{ Game.new() |
+      paragraph: "Hello Typeracer, we're MakingDevs."
+    } |> Game.add_player("neodevelop")
+    assert game.paragraph == "Hello Typeracer, we're MakingDevs."
+
+    game = Game.plays game, "neodevelop", "Hello"
+    player = Enum.find(game.players, fn %Player{username: "neodevelop"} -> :ok end)
     assert player.paragraph_typed == "Hello"
     assert player.score == 15
+    assert Enum.count(game.players) == 1
   end
 
 end
