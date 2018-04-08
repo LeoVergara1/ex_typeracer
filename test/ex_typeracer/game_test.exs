fb75de11d8f618d5fe664b67ba87b20ce6e165b2
neodevelop <neodevelop@gmail.com>
2018-04-08 13:58:32 -0500

    #16 add 2 test for game

--- test/ex_typeracer/game_test.exs
+++ test/ex_typeracer/game_test.exs
@@ -3,10 +3,29 @@ defmodule ExTyperacer.Structs.GameTest do
   alias ExTyperacer.Structs.Game
   doctest Game
 
+  @doc """
+  We must remove the dependency for obtain text
+  """
   test "a new game" do
     game = Game.new()
     assert game.paragraph
     assert Enum.count(game.players) == 0
+  end
+
+  test "add a new player" do
+    game = Game.new()
+    updated_game = Game.add_player(game, "neodevelop")
+    assert Enum.count(updated_game.players) == 1
+  end
+
+  test "add the same player twice" do
+  end
+
+  test "playing the game" do
+    game = %{ Game.new() | paragraph: "Hello Typeracer, we're MakingDevs." }
+    updated_game = Game.add_player(game, "neodevelop")
+    assert updated_game.paragraph == "Hello Typeracer, we're MakingDevs."
+    _updated_game = Game.plays game, "neodevelop", "Hello"
   end
 
 end
