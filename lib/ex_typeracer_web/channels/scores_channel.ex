6553d4e413dd56e7c920ff72c45a6afd68d30382
Brandon Vergara <brandon@makingdevs.com>
2018-04-10 12:49:24 -0500

    #18 return socores updates

--- lib/ex_typeracer_web/channels/scores_channel.ex
+++ lib/ex_typeracer_web/channels/scores_channel.ex
@@ -12,16 +12,12 @@ defmodule ExTyperacerWeb.ScoresChannel do
 
 	def handle_in("scores:set", payload, socket) do
     Logger.warn " ::::::::: Scores:Set :::::::: Insert score"
-    IO.inspect payload["uuid"]
     [{_,game}] = :ets.lookup(:"#{payload["uuid"]}","game")
-    IO.inspect payload
     player = Enum.find(game.players, fn %Player{username: u} -> u == payload["user"] end)
-    IO.inspect player
-    IO.inspect payload["score"]
     player = Player.update_socere_player(player, payload["score"])
     game = Game.update_socere_player(game, player)
     :ets.insert(:"#{game.uuid}", {"game", game} )
-		broadcast! socket, "scores:show", payload
+		broadcast! socket, "scores:show", %{"game" => game.players}
     {:noreply, socket}
 	end
 
