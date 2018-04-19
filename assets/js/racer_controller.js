502f79221753343079fe07a3dc4e0bda2d6ff601
Brandon Vergara <brandon@makingdevs.com>
2018-04-19 09:14:16 -0500

    11 creating function to animate sprite

--- assets/js/racer_controller.js
+++ assets/js/racer_controller.js
@@ -2,6 +2,7 @@ import socket from "./socket"
 import HandlebarsResolver from "./handlebars_resolver"
 import { MatchController } from "./match_controller";
 import "sprite-js"
+import "./sprite"
 
 export var RacerController = {
   uuid: Math.floor((Math.random() * 10000) + 1),
@@ -162,11 +163,24 @@ export var RacerController = {
   },
 
   animattionSprite: function () {
+    $(".scott").animateSprite({
+      fps: 12,
+      animations: {
+          walkRight: [0, 1, 2, 3, 4, 5, 6, 7],
+          walkLeft: [15, 14, 13, 12, 11, 10, 9, 8]
+      },
+      loop: true,
+      complete: function(){
+          // use complete only when you set animations with 'loop: false'
+          alert("animation End");
+      }
+  });
   },
 
   scenePlayer: function() {
     var image = new Image()
     image.src = '/images/pan.png'
+    console.log(image)
     var sprite = new Sprite({
       canvas: document.getElementById('canvas'),
       image: image,
