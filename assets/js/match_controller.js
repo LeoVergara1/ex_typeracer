a1d952b962691a11049c880b30caf25d1b8c878b
Brandon Vergara <brandon@makingdevs.com>
2018-05-04 15:06:04 -0500

    28 fixed bug in firefox

--- assets/js/match_controller.js
+++ assets/js/match_controller.js
@@ -12,19 +12,27 @@ export var MatchController = {
     let score = 0
     let textArea = textRandoom
     //console.log(textRandoom)
+    let statusDead = 1
     $("#currentWord").text(textArea.charAt(nextWord))
     lastWord = textArea.length
     $("#pressKey").on("keydown", (event)=>{
+      let notDead = 1
       if (textArea.charAt(nextWord) === " "){
        // console.log("Espacio")
         $("#currentWord").text("Espacio")
         $("#pressKey").val("")
       }
+      if (event.key === 'Dead' && statusDead == 1){
+        $("#currentWord").text(textArea.charAt(nextWord))
+        textCurent +=textArea.charAt(nextWord)
+        nextWord +=1
+        notDead = 0
+      }
       else {
         $("#currentWord").text(textArea.charAt(nextWord))
       }
       //console.log(`Presiona : ${event.key}, Numero de palabra: ${nextWord} , Letra del parrafo: ${textArea.charAt(nextWord)}`)
-      if(event.key == textArea.charAt(nextWord)){
+      if(event.key == textArea.charAt(nextWord) && notDead == 1){
         textCurent +=textArea.charAt(nextWord)
         nextWord +=1
         score = (nextWord * 100) / (textArea.length)
