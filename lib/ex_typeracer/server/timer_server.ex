4f233b24af497eade6f24f6e50470f505c97168a
Brandon Vergara <brandon@makingdevs.com>
2018-05-07 10:35:47 -0500

    #29 creating function to start timer for waiting game

--- lib/ex_typeracer/server/timer_server.ex
+++ lib/ex_typeracer/server/timer_server.ex
@@ -38,23 +38,28 @@ defmodule ExTyperacer.TimerServer do
   end
 
   def handle_info({:waiting, 0, uuid}, state) do
-    IO.puts "Finished proccess"
-    broadcast 0, %{message: "Counting", uuid: uuid, select: "new_time_"}
+    IO.puts "Finished procces of waiting"
+    broadcast 0, %{message: "Counting", uuid: uuid, select: "waiting_time_"}
     {:noreply, state}
   end
 
   def handle_info({:waiting, counter, uuid}, state) do
-    IO.puts "Counting..."
+    IO.puts "waiting..."
     IO.inspect uuid
     IO.inspect counter
-    broadcast counter, %{message: "Counting", uuid: uuid, select: "new_time_"}
+    broadcast counter, %{message: "waiting", uuid: uuid, select: "waiting_time_"}
     counter = counter - 1
-    timer = Process.send_after(self(), {:work, counter, uuid},1_000)
+    timer = Process.send_after(self(), {:waiting, counter, uuid},1_000)
     {:noreply, %{timer: timer}}
   end
 
   def handle_info({:start_timer, counter, uuid}, state) do
     timer = Process.send_after(self(), {:work, counter, uuid}, 1_000)
+    {:noreply, %{timer: timer}}
+  end
+
+  def handle_info({:start_timer_waiting, counter, uuid}, state) do
+    timer = Process.send_after(self(), {:waiting, counter, uuid}, 1_000)
     {:noreply, %{timer: timer}}
   end
     # So that unhanded messages don't error
