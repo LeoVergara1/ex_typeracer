8b0f6e1ea11b433e187653bf86179cbd5d219170
Brandon Vergara <brandon@makingdevs.com>
2018-05-09 16:44:00 -0500

    #31 adding param username to be saved

--- lib/ex_typeracer_web/controllers/page_controller.ex
+++ lib/ex_typeracer_web/controllers/page_controller.ex
@@ -13,7 +13,7 @@ defmodule ExTyperacerWeb.PageController do
 
   def new_user(conn, params) do
     IO.inspect params["person"]["password"]
-    Repo.insert %Person{name: params["person"]["name"], lastname: params["person"]["lastname"], email: params["person"]["email"], password: params["person"]["password"]}
+    Repo.insert %Person{name: params["person"]["name"], lastname: params["person"]["lastname"], email: params["person"]["email"], password: params["person"]["password"], username: params["person"]["username"]}
     render conn, "index.html" 
   end
 end
