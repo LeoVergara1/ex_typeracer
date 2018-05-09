99ebdbf0d202aa402d5d4d7b69a104fc5e3f60ac
Brandon Vergara <brandon@makingdevs.com>
2018-05-09 16:11:50 -0500

    #31 saved register from form

--- lib/ex_typeracer_web/controllers/page_controller.ex
+++ lib/ex_typeracer_web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule ExTyperacerWeb.PageController do
   use ExTyperacerWeb, :controller
-  
+  alias ExTyperacer.Repo
+  alias ExTyperacer.Person
+
   def index(conn, _params) do
     render conn, "index.html"
   end
@@ -10,7 +12,8 @@ defmodule ExTyperacerWeb.PageController do
   end 
 
   def new_user(conn, params) do
-    IO.inspect params
+    IO.inspect params["person"]["password"]
+    Repo.insert %Person{name: params["person"]["name"], lastname: params["person"]["lastname"], email: params["person"]["email"], password: params["person"]["password"]}
     render conn, "index.html" 
   end
 end
