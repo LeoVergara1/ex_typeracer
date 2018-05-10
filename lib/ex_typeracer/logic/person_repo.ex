95d24c72f70b63b5a53deec28adb43a7b6076f88
Brandon Vergara <brandon@makingdevs.com>
2018-05-10 16:31:45 -0500

    #32 addgin query to get person per username

--- lib/ex_typeracer/logic/person_repo.ex
+++ lib/ex_typeracer/logic/person_repo.ex
@@ -2,6 +2,7 @@ defmodule ExTyperacer.Logic.PersonRepo do
 
   alias ExTyperacer.Repo
   alias ExTyperacer.Person
+  import Ecto.Query, only: [from: 2]
 
   def save_person(person) do
     changeset = Person.changeset( %Person{}, Map.from_struct(person))
@@ -9,5 +10,10 @@ defmodule ExTyperacer.Logic.PersonRepo do
       true -> Repo.insert changeset
       false -> changeset.errors
     end
+  end
+
+  def find_user_by_username(username) do
+    query = from u in Person, where: u.username == ^username, select: u
+    Repo.all(query)
   end
 end
