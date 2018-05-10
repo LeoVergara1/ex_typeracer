d85a74c872a473b52e88285ad71e9e17c740c7d8
Brandon Vergara <brandon@makingdevs.com>
2018-05-10 16:01:02 -0500

    #32 adding encripty to password

--- lib/ex_typeracer/person.ex
+++ lib/ex_typeracer/person.ex
@@ -1,6 +1,7 @@
 defmodule ExTyperacer.Person do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Comeonin.Bcrypt
 
 
   schema "persons" do
@@ -19,5 +20,12 @@ defmodule ExTyperacer.Person do
     |> validate_required([:name, :lastname, :email, :password, :username])
     |> unsafe_validate_unique([:email], ExTyperacer.Repo, message: "Email already registered")
     |> unsafe_validate_unique([:username], ExTyperacer.Repo, message: "Username already registered")
+    |> put_pass_hash() 
   end
+
+  defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, password: Bcrypt.hashpwsalt(password))
+  end
+
+  defp put_pass_hash(changeset), do: changeset
 end
