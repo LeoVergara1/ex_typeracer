65a929f3cc1506e3b60ec74d8ac46681a06f63a9
Brandon Vergara <brandon@makingdevs.com>
2018-05-10 16:40:51 -0500

    #32 adding check password

--- lib/ex_typeracer/logic/person_repo.ex
+++ lib/ex_typeracer/logic/person_repo.ex
@@ -3,6 +3,7 @@ defmodule ExTyperacer.Logic.PersonRepo do
   alias ExTyperacer.Repo
   alias ExTyperacer.Person
   import Ecto.Query, only: [from: 2]
+  alias Comeonin.Bcrypt
 
   def save_person(person) do
     changeset = Person.changeset( %Person{}, Map.from_struct(person))
@@ -16,4 +17,12 @@ defmodule ExTyperacer.Logic.PersonRepo do
     query = from u in Person, where: u.username == ^username, select: u
     Repo.all(query)
   end
+
+  def check_password(person, password) do
+    case Bcrypt.checkpw(password, person.password) do
+      true -> {:ok, person}
+      false -> {:error, "Incorrect username or password"}
+    end
+  end
+
 end
