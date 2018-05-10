7e2b1640aafc6cbad140761e418723ee9bca7fc9
Brandon Vergara <brandon@makingdevs.com>
2018-05-10 11:29:51 -0500

    #32 refactor to method

--- lib/ex_typeracer_web/controllers/page_controller.ex
+++ lib/ex_typeracer_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule ExTyperacerWeb.PageController do
   use ExTyperacerWeb, :controller
   alias ExTyperacer.Repo
   alias ExTyperacer.Person
+  alias ExTyperacer.Logic.PersonRepo
 
   def index(conn, _params) do
     render conn, "index.html"
@@ -12,8 +13,11 @@ defmodule ExTyperacerWeb.PageController do
   end 
 
   def new_user(conn, params) do
-    IO.inspect params["person"]["password"]
-    Repo.insert %Person{name: params["person"]["name"], lastname: params["person"]["lastname"], email: params["person"]["email"], password: params["person"]["password"], username: params["person"]["username"]}
+    #IO.inspect params["person"]
+    #IO.inspect params["person"]["password"]
+    #Map.merge{%Person{}, params["person"]}
+    %Person{name: params["person"]["name"], lastname: params["person"]["lastname"], email: params["person"]["email"], password: params["person"]["password"], username: params["person"]["username"]}
+    |> PersonRepo.save_person
     render conn, "index.html" 
   end
 end
