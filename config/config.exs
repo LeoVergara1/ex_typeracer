fd4c27c57e08adee9a316fde5b8be5beca901b9e
Brandon Vergara <brandon@makingdevs.com>
2018-05-11 08:44:16 -0500

    #32 addding config to guardian

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,12 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:user_id]
 
+#Configures Guardian
+
+config :ex_typeracer, ExTyperacer.Auth.Guardian,
+  issuer: "auth_ex", # Name of your app/company/product
+  secret_key: "AVp1NjzxTCrf5467/cGIjkeRxLOkKp5k1b6z9HjkLX0L3L5f5UVHE9uf3MkxnPNE" # Replace this with the output of the mix command
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
