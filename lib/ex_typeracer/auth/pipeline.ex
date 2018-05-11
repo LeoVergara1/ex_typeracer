2cedd94aefaec70e9a6f8d17fda659dfa0db939e
Brandon Vergara <brandon@makingdevs.com>
2018-05-11 08:45:26 -0500

    #32 adding case when failed

---
+++ lib/ex_typeracer/auth/pipeline.ex
@@ -0,0 +1,12 @@
+defmodule ExTyperacer.Auth.Pipeline do
+  use Guardian.Plug.Pipeline,
+    otp_app: :ex_typeracer,
+    error_handler: AuthEx.Auth.ErrorHandler,
+    module: ExTyperacer.Auth.Guardian
+  # If there is a session token, validate it
+  plug Guardian.Plug.VerifySession, claims: %{"typ" => "access"}
+  # If there is an authorization header, validate it
+  plug Guardian.Plug.VerifyHeader, claims: %{"typ" => "access"}
+  # Load the user if either of the verifications worked
+  plug Guardian.Plug.LoadResource, allow_blank: true
+end
