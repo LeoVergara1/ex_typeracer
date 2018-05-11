524c08470010fafd887763bdd40c64f1d9ea50c7
Brandon Vergara <brandon@makingdevs.com>
2018-05-11 11:14:35 -0500

    #32 validate if there user in logged

--- lib/ex_typeracer_web/controllers/page_controller.ex
+++ lib/ex_typeracer_web/controllers/page_controller.ex
@@ -3,8 +3,16 @@ defmodule ExTyperacerWeb.PageController do
   alias ExTyperacer.Repo
   alias ExTyperacer.Person
   alias ExTyperacer.Logic.PersonRepo
+  alias ExTyperacer.Auth.Guardian
 
   def index(conn, _params) do
+    changeset = PersonRepo.change_user(%Person{})
+    maybe_user = Guardian.Plug.current_resource(conn)
+    message = if maybe_user != nil do
+      IO.puts "Someone is logged in"
+    else
+      IO.puts "No one is logged in"
+    end
     render conn, "index.html"
   end
 
