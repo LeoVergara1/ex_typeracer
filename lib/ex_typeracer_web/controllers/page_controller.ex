456001fcda2283662e372c13fb7798ab77a8fcf5
Brandon Vergara <brandon@makingdevs.com>
2018-05-11 12:13:04 -0500

    #32 adding methods of authentification

--- lib/ex_typeracer_web/controllers/page_controller.ex
+++ lib/ex_typeracer_web/controllers/page_controller.ex
@@ -13,7 +13,37 @@ defmodule ExTyperacerWeb.PageController do
     else
       IO.puts "No one is logged in"
     end
-    render conn, "index.html"
+    conn
+      |> put_flash(:info, message)
+      |> render("index.html", changeset: changeset, action: page_path(conn, :login), maybe_user: maybe_user)
+  end
+
+  def login(conn, %{"person" => %{"username" => username, "password" => password}}) do
+    PersonRepo.authenticate_user(username, password)
+    |> login_reply(conn)
+  end
+
+  defp login_reply({:error, error}, conn) do
+    conn
+    |> put_flash(:error, error)
+    |> redirect(to: "/")
+  end
+
+  defp login_reply({:ok, person}, conn) do
+    conn
+    |> put_flash(:success, "Welcome back!")
+    |> Guardian.Plug.sign_in(person)
+    |> redirect(to: "/")
+  end
+
+  def logout(conn, _) do
+    conn
+    |> Guardian.Plug.sign_out()
+    |> redirect(to: page_path(conn, :login))
+  end
+
+  def secret(conn, _params) do
+    render(conn, "secret.html")
   end
 
   def racer(conn, _params) do
