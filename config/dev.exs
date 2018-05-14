6864c1dba90dd59af6b0a32626f59627d2285e7b
Brandon Vergara <brandon@makingdevs.com>
2018-05-14 15:14:40 -0500

    #34 generate certificate fake ssl

--- config/dev.exs
+++ config/dev.exs
@@ -8,6 +8,10 @@ use Mix.Config
 # with brunch.io to recompile .js and .css sources.
 config :ex_typeracer, ExTyperacerWeb.Endpoint,
   http: [port: 4000],
+  https: [port: 4443,
+          otp_app: :ex_typeracer,
+          keyfile: "priv/keys/localhost.key",
+          certfile: "priv/keys/localhost.cert"],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
