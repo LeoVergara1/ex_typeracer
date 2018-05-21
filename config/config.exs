1dd7f3ab0edb3aa68c13bf484c0468cf0bd8cd73
Brandon Vergara <brandon@makingdevs.com>
2018-05-21 10:56:07 -0500

    #42 adding config to adapter Bambo from gmail

--- config/config.exs
+++ config/config.exs
@@ -42,4 +42,18 @@ config :ueberauth, Ueberauth.Strategy.Facebook.OAuth,
 client_id: System.get_env("FACEBOOK_APP_ID"),
 client_secret: System.get_env("FACEBOOK_APP_SECRET")
 
+
+#Config adapter email
+
+config :ex_typeracer, ExTyperacer.Service.Mailer,
+  adapter: Bamboo.SMTPAdapter,
+  server: "smtp.gmail.com",
+  port: 587,
+  username: "brandon@makingdevs.com",
+  password: "Chivasleo@x1",
+  tls: :if_available, # can be `:always` or `:never`
+  allowed_tls_versions: [:"tlsv1", :"tlsv1.1", :"tlsv1.2"], # or {":system", ALLOWED_TLS_VERSIONS"} w/ comma seprated values (e.g. "tlsv1.1,tlsv1.2")
+  ssl: false, # can be `true`
+  retries: 1 
+
 import_config "#{Mix.env}.exs"
