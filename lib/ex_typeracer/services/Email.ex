5128f09427066c12ed25dffd7cee14bc795620fc
Brandon Vergara <brandon@makingdevs.com>
2018-05-21 11:35:20 -0500

    #42 creating method to send email whe it was registered

--- lib/ex_typeracer/services/Email.ex
+++ lib/ex_typeracer/services/Email.ex
@@ -1,5 +1,6 @@
 defmodule ExTyperacer.Service.Email do
   import Bamboo.Email
+  import Bamboo.Phoenix
 
   def welcome_email do
     new_email(
@@ -9,5 +10,22 @@ defmodule ExTyperacer.Service.Email do
       html_body: "<strong>Thanks for joining!</strong>",
       text_body: "Thanks for joining!"
     )
+  end
+
+  def send_email_register(email, password) do
+    base_email
+    |> to(email)
+    |> subject("Welcome!!!")
+    |> put_header("Reply-To", "someone@example.com")
+    |> html_body("<strong>bienvenido esta es tu contraseña: #{password}</strong>")
+    |> text_body("Welcome esta es tu contraseña: #{password}")
+  end
+
+  defp base_email do
+    # Here you can set a default from, default headers, etc.
+    new_email
+    |> from("brandon@makingdevs.com")
+    |> put_html_layout({ExTyperacer.LayoutView, "email.html"})
+    |> put_text_layout({ExTyperacer.LayoutView, "email.text"})
   end
 end
