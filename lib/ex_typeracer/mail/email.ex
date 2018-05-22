def423b2fb569303010e6f882e4bef4c1a06ea2b
Brandon Vergara <brandon@makingdevs.com>
2018-05-22 11:22:41 -0500

    #44 creating function to send email with token

--- lib/ex_typeracer/mail/email.ex
+++ lib/ex_typeracer/mail/email.ex
@@ -21,6 +21,15 @@ defmodule ExTyperacer.Mail.Email do
     |> text_body("Welcome esta es tu contraseña: #{password}")
   end
 
+  def send_email_recovery(email, token, username) do
+    base_email
+    |> to(email)
+    |> subject("Recuperación de contraseña")
+    |> put_header("Reply-To", "someone@example.com")
+    |> html_body("<strong>Link para restaurar contraseña: #{ExTyperacerWeb.Endpoint.url}/#{token}/#{username}</strong>")
+    |> text_body("Welcome esta es tu contraseña")
+  end
+
   defp base_email do
     # Here you can set a default from, default headers, etc.
     new_email
@@ -28,4 +37,5 @@ defmodule ExTyperacer.Mail.Email do
     |> put_html_layout({ExTyperacer.LayoutView, "email.html"})
     |> put_text_layout({ExTyperacer.LayoutView, "email.text"})
   end
+
 end
