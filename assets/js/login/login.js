ffafc5912e1c7d0117a029ee1be527d2c82c5165
Brandon Vergara <brandon@makingdevs.com>
2018-06-08 08:21:39 -0500

    #51 adding function to recovery password

--- assets/js/login/login.js
+++ assets/js/login/login.js
@@ -1,1 +1,54 @@
-console.log("holalla")
+import socket from "../global/socket"
+import "bootstrap-notify"
+
+export var LoginController ={
+  channelRoom: null,
+  initChannelRoom: function () {
+    $("#container_timer_waiting").hide()
+    this.channelRoom = socket.channel("room:new", {})
+    let that = this
+    this.channelRoom.join()
+			.receive("ok", resp => {
+        console.log("Room successfully 😎", resp)
+        this.channelRoom.push("get_romms")
+			})
+      .receive("error", resp => { console.log("Unable to join", resp) })
+	  this.channelRoom.on("list_rooms", msg => {
+				$("#list_roms").html("")
+					$.each(msg.rooms, function( index, value ) {
+            $("#list_roms").append(`<p><i class="fa fa-cloud "></i><strong > Sala: </strong> <span class="pointer room_ref">${value}</span></p> `)
+					});
+			});
+  },
+recoveryPass: function() {
+    $("#recovery_button").on("click", ()=> {
+      let email = $("#validEmail_pass").val()
+      console.log(email)
+      $("#modalPassword").modal("hide")
+      this.channelRoom.push("recovery_pass", email)
+        .receive('ok', resp => { console.log(resp)
+          if (resp.existed){
+            $.notify({message: 'Datos enviados' },{type: 'danger'});
+          }
+          else {
+            $.notify({message: 'El correo no existe' },{type: 'danger'});
+          }
+        })
+    })
+  },
+
+  bindEvents:function (){
+    console.log("Init envents Logind")
+    this.initChannelRoom()
+    this.recoveryPass()
+  },
+
+  testContext: function(){
+  },
+
+  start: function(){
+    this.bindEvents()
+  }
+}
+
+LoginController.start()
