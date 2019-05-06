d69ea480154a9b6c98fb43d5e3ad7ee1bf2e2cb8
Brandon Vergara <brandon@makingdevs.com>
2019-05-06 12:29:47 -0500

    coding master adding cas plugin

---
+++ bin/main/mx/edu/ebc/comisiones/controllers/AdministrarionController.groovy
@@ -0,0 +1,20 @@
+
+package mx.edu.ebc.comisiones.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.servlet.ModelAndView
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class AdministrarionController {
+
+   @RequestMapping("/")
+   @ResponseBody
+   public ModelAndView home() {
+		 	ModelAndView model = new ModelAndView("home");
+			model.addObject("title", "Baeldung");
+			return model
+   }
+}
