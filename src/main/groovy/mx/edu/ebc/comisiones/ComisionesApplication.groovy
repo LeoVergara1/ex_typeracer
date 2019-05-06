664ad0f81dddd110307ec48f2ba2051760213671
Brandon Vergara <brandon@makingdevs.com>
2019-05-06 09:58:54 -0500

    coding Master stating project

---
+++ src/main/groovy/mx/edu/ebc/comisiones/ComisionesApplication.groovy
@@ -0,0 +1,13 @@
+package mx.edu.ebc.comisiones
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ComisionesApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ComisionesApplication, args)
+	}
+
+}
