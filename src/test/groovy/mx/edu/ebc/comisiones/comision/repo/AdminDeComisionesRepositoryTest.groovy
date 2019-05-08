a961762356661401b493383223f860fa49a82825
Brandon Vergara <brandon@makingdevs.com>
2019-05-08 15:38:29 -0500

    coding CP-1   adding text to comission repository

---
+++ src/test/groovy/mx/edu/ebc/comisiones/comision/repo/AdminDeComisionesRepositoryTest.groovy
@@ -0,0 +1,28 @@
+package mx.edu.ebc.comisiones.comision.repo
+
+import mx.edu.ebc.comisiones.comision.domain.AdminDeComisiones;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.junit4.SpringRunner
+import org.junit.runner.RunWith
+import org.junit.Test
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class AdminDeComisionesRepositoryIntegrationTest {
+
+	@Autowired
+	AdminDeComisionesRepository adminDeComisionesRepository
+
+	@Test
+	public void getAllComisiones() {
+	    // given
+	    // when
+	    def found = adminDeComisionesRepository.findAll()
+	    // then
+			assertThat(found).isNotNull()
+	}
+}
