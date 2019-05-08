7dd5888bf164486faed96db300a12329480e0344
Brandon Vergara <brandon@makingdevs.com>
2019-05-08 16:36:01 -0500

    coding CP-1   adding services to promoters

--- src/main/groovy/mx/edu/ebc/comisiones/services/AdministrationServiceImpl.groovy
+++ src/main/groovy/mx/edu/ebc/comisiones/services/AdministrationServiceImpl.groovy
@@ -2,7 +2,9 @@ package mx.edu.ebc.comisiones.services
 
 import org.springframework.stereotype.Service
 import mx.edu.ebc.comisiones.comision.repo.AdminDeComisionesRepository
+import mx.edu.ebc.comisiones.comision.repo.PromoterAssociationRepository
 import mx.edu.ebc.comisiones.comision.domain.AdminDeComisiones;
+import mx.edu.ebc.comisiones.comision.domain.PromoterAssociation;
 import org.springframework.beans.factory.annotation.Autowired
 
 @Service
@@ -10,9 +12,16 @@ public class AdministrationServiceImpl implements AdministrationService {
 
 	@Autowired
 	AdminDeComisionesRepository adminDeComisionesRepository
+	@Autowired
+	PromoterAssociationRepository promoterAssociationRepository
 
 	@Override
 	List<AdminDeComisiones> findAllComission(){
 		adminDeComisionesRepository.findAll()
+	}
+
+	@Override
+	List<PromoterAssociation> findAllPromoters(){
+		promoterAssociationRepository.findAll()
 	}
 }
