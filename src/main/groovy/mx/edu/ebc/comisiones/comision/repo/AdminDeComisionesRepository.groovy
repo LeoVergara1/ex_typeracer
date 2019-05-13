2e3af818c5034db9fb1656d7da53152f69b1e604
Brandon Vergara <brandon@makingdevs.com>
2019-05-13 12:19:55 -0500

    coding CP-2   adding method to findBy id

--- src/main/groovy/mx/edu/ebc/comisiones/comision/repo/AdminDeComisionesRepository.groovy
+++ src/main/groovy/mx/edu/ebc/comisiones/comision/repo/AdminDeComisionesRepository.groovy
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository
 @Repository
 public interface AdminDeComisionesRepository extends JpaRepository<AdminDeComisiones,Integer> {
 	List<AdminDeComisiones> findAll()
+	AdminDeComisiones findById(Integer id)
 }
