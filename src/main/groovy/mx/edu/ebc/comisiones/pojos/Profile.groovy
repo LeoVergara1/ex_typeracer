87d07c5300c91ffcefe7a72d14f338458a7ed1d2
Brandon Vergara <brandon@makingdevs.com>
2019-05-17 16:09:09 -0500

    coding CP-6   adding profile to parser response

--- src/main/groovy/mx/edu/ebc/comisiones/pojos/Profile.groovy
+++ src/main/groovy/mx/edu/ebc/comisiones/pojos/Profile.groovy
@@ -1,9 +1,24 @@
 package mx.edu.ebc.comisiones.pojos
 
+import wslite.json.JSONObject
+import static java.util.Calendar.*
+import java.time.LocalDate
+
 class Profile {
   Long id
   String description
   String enrollment
   String fullName
-  Date dateCreated
+  LocalDate dateCreated
+
+    static Profile fromJsonObject(JSONObject jsonObject){
+      new Profile(
+                   id:jsonObject?.id,
+                   description:jsonObject?.description,
+                   enrollment:jsonObject?.enrollment,
+                   fullName:jsonObject?.fullName,
+                   dateCreated: jsonObject?.dateCreated ?  LocalDate.parse(jsonObject.dateCreated) : LocalDate.now()
+                  // dateCreated:new Date().parse('yyy-mm-dd',jsonObject?.dateCreated ?: "0000-00-00")
+                        )
+  }
 }
