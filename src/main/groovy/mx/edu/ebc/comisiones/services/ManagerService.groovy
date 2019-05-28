6ed378cabf7b66eba6d6e76b9126e1ab328255b1
Brandon Vergara <brandon@makingdevs.com>
2019-05-28 11:39:26 -0500

    coding CP-1   creating services to create a manager

--- src/main/groovy/mx/edu/ebc/comisiones/services/ManagerService.groovy
+++ src/main/groovy/mx/edu/ebc/comisiones/services/ManagerService.groovy
@@ -1,6 +1,6 @@
 package mx.edu.ebc.comisiones.services
 
 interface ManagerService {
-  boolean createManager(String userName, Long pidm, String recrCode)
+  Map createManager(String userName, Long pidm, String recrCode)
   boolean deleteManager(String userName)
 }
--- src/main/groovy/mx/edu/ebc/comisiones/services/ManagerServiceImpl.groovy
+++ src/main/groovy/mx/edu/ebc/comisiones/services/ManagerServiceImpl.groovy
@@ -5,28 +5,52 @@ import org.springframework.stereotype.Service
 import org.springframework.beans.factory.annotation.Value
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import mx.edu.ebc.comisiones.comision.domain.ProgramManager
+import mx.edu.ebc.comisiones.comision.domain.PidmAndUserNamePK
+import mx.edu.ebc.comisiones.comision.repo.ProgramManagerRepository
 
 @Service
 class ManagerServiceImpl implements ManagerService {
 
   Logger logger = LoggerFactory.getLogger(ManagerServiceImpl.class)
+  private static String ALREADY_EXISTS = "ERROR, this relation already exists"
+  private static String RECR_CODE = "ERROR, this recruiter code already exists"
+  private static String NOT_FOUND = "ERROR, this promoter does not exists"
+  private static String MANAGER_NOT_FOUND = "ERROR, this manager does not exists"
+  private static String DB_ERROR = "ERROR, data base error"
+  private static String SUCCESS = "Operation succesfully achieved..."
 
   @Autowired
   RestConnectionService restConnectionService
 	@Value('${url.apicomisiones}')
 	String clientComissions
+  @Autowired
+  ProgramManagerRepository programManagerRepository
+  @Autowired
+  PromoterService promoterService
 
   @Override
-  boolean createManager(String userName, Long pidm, String recrCode) {
-    logger.info "Saving new manager: $userName"
-    def postStatus = restConnectionService.post(
-            clientComissions,
-            "/v1/api/manager/program/",
-            [user_name: userName,
-             pidm: pidm,
-             recr_code: recrCode])
-    postStatus?: logger.error("ERROR creating manager: $userName)")
-    postStatus ? true : false
+  Map createManager(String userName, Long pidm, String recrCode) {
+    logger.info "Creating new Program Manager: $userName with pidm: $pidm and recrCode: $recrCode"
+    PidmAndUserNamePK id = new PidmAndUserNamePK(pidm: pidm, userName: userName, recrCode: recrCode)
+    if (programManagerRepository.findOneById_RecrCode(recrCode) || promoterService.isAPromoterSavedWithRecrCode(recrCode)) {
+      logger.info RECR_CODE
+      return [message: RECR_CODE, created: false]
+    }
+    if(programManagerRepository.findOneById_UserName(userName)){
+      logger.error(ALREADY_EXISTS)
+      return [message: ALREADY_EXISTS, created: false]
+    }
+    ProgramManager programManager = new ProgramManager(id: id)
+    programManager = programManagerRepository.save(programManager)
+    if (programManager){
+      logger.info("Successfuly created...")
+      [message: "Successfuly created...", created: true]
+    }
+    else{
+      logger.error(DB_ERROR)
+      [message: DB_ERROR, created: false]
+    }
   }
 
   @Override
