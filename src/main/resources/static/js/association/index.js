70d0a75d86fde9da189147db2aaf438068f103cd
Brandon Vergara <brandon@makingdevs.com>
2019-05-29 15:57:57 -0500

    coding CP-6   creating view to association

---
+++ src/main/resources/static/js/association/index.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    username: document.getElementById("username").value,
+    person: Object,
+    headerBgVariant: "white",
+    headerTextVariant: 'dark'
+  },
+  created: function() {
+    this.$http.post('/administration/search/association', {user: this.username}).then(response => {
+      // get body data
+      console.log(response.body);
+      this.person = response.body.person
+    }, response => {
+      console.log("Fail")
+      console.log(response)
+      // error callback
+    })
+  },
+  methods:{
+    update: function (numero) {
+      console.log(numero)
+    },
+    sendAssociation: function() {
+      console.log("Click")
+      this.$bvModal.show("modal-1")
+    },
+    closedModal: function(){
+      this.$bvModal.hide("modal-1")
+    }
+  }
+})
