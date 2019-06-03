67c887ba2acc7b93cc755975dc694043bea91e97
Brandon Vergara <brandon@makingdevs.com>
2019-06-03 12:46:11 -0500

    coding CP-20   I created configuration to consummer stored procedure

---
+++ src/main/groovy/mx/edu/ebc/comisiones/comision/domain/PaymentComission.groovy
@@ -0,0 +1,23 @@
+package mx.edu.ebc.comisiones.comision.domain
+
+import javax.persistence.*
+
+@Entity
+@NamedStoredProcedureQueries([
+    @NamedStoredProcedureQuery(
+        name = "java_procedure_name",
+        procedureName = "genpub.pc_pago_comisiones.pr_comisiones",
+        parameters = [
+          @StoredProcedureParameter(mode=ParameterMode.IN, name="p_fecha_pago_ini", type=Date.class),
+          @StoredProcedureParameter(mode=ParameterMode.IN, name="p_fecha_pago_fin", type=Date.class),
+          @StoredProcedureParameter(mode=ParameterMode.IN, name="p_campus", type=String.class),
+          @StoredProcedureParameter(type = void.class, mode = ParameterMode.REF_CURSOR)
+		])
+])
+public class PaymentComission implements Serializable {
+
+	@Id
+	@Column(name = "id_promotor")
+	Long id
+
+}
