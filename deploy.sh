46a033317b746a6221c92fbbdec0465e3ea6faac
luisito <luis@makingdevs.com>
2019-06-03 16:39:09 -0500

    coding CP-21 Change port

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ docker rm $PROJECT_NAME
 
 docker pull 172.31.100.23:5000/ebc/springboot/$PROJECT_NAME
 
-docker run -dit --restart unless-stopped --network general-ebc-network --name $PROJECT_NAME -d -p $PORT:8105 172.31.100.23:5000/ebc/springboot/$PROJECT_NAME
+docker run -dit --restart unless-stopped --network general-ebc-network --name $PROJECT_NAME -d -p $PORT:8090 172.31.100.23:5000/ebc/springboot/$PROJECT_NAME
