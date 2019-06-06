2615110dc142bbc864d9e6449073bf5b24fac639
Brandon Vergara <brandon@makingdevs.com>
2019-06-06 16:26:02 -0500

    coding CP-20  I created UserDetails to impl user

---
+++ src/main/groovy/mx/edu/ebc/comisiones/services/UserDetailsServiceImpl.groovy
@@ -0,0 +1,21 @@
+package mx.edu.ebc.comisiones.services
+
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.core.userdetails.UserDetails
+import mx.edu.ebc.comisiones.pojos.EbcUser
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    // ...
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+      EbcUser user = new EbcUser()
+        if (user == null) {
+            throw new UsernameNotFoundException(
+              String.format("Username not found for domain, username=%s, domain=%s"));
+        }
+        return user;
+    }
+}
