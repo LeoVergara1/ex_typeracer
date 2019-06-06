8e14b500df8d410e599df8cb1a21118b886cac2b
Brandon Vergara <brandon@makingdevs.com>
2019-06-06 16:25:28 -0500

    coding CP-20  I created functionality to add object ebc to session

--- src/main/groovy/mx/edu/ebc/comisiones/interceptors/CasInterceptor.groovy
+++ src/main/groovy/mx/edu/ebc/comisiones/interceptors/CasInterceptor.groovy
@@ -6,14 +6,43 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import java.security.Principal
+import org.springframework.beans.factory.annotation.Value
+import mx.edu.ebc.comisiones.services.RestConnectionService
+import org.springframework.security.core.userdetails.UserDetailsService
+import mx.edu.ebc.comisiones.services.PersonService
+import mx.edu.ebc.comisiones.pojos.Profile
+import mx.edu.ebc.comisiones.pojos.EbcUser
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.SessionAttributes
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Component
 class CasInterceptor implements HandlerInterceptor {
 
-   @Override
-   boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+  @Value('${url.apibannerseguridad}')
+  String clientApiBannerSeguridad
+  String roles = "v2/api/user/profile"
+  String profiles = "v2/api/user/role"
+  @Autowired
+  RestConnectionService restConnectionService
+  @Autowired
+  PersonService personService
 
-      System.out.println("Pre Handle method is Calling");
-      return true;
-   }
+  @Override
+  boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+     def userName = request.getUserPrincipal().getUserDetails()
+     def session = request.getSession()
+     (session.getAttribute("ebcUser")) ? 'Nothing' : addUserEbcTosession(userName.username, session)
+     System.out.println("Pre Handle method is Calling");
+     return true;
+  }
+
+  void addUserEbcTosession(String userName, def session){
+    println "Adding userEbc"
+    session.setAttribute("ebcUser", new EbcUser())
+    List<Profile> profiles = personService.findPersonByUsernameAndPortalName(userName, "Pago%20de%20comisiones")
+    println profiles
+  }
+
 }
