22bf49b4b5bc5d31cd66bc453b3c41d01ca72f3c
Brandon Vergara <brandon@makingdevs.com>
2019-06-06 16:25:51 -0500

    coding CP-20  I created UserDetails to impl user

--- src/main/groovy/mx/edu/ebc/comisiones/pojos/EbcUser.groovy
+++ src/main/groovy/mx/edu/ebc/comisiones/pojos/EbcUser.groovy
@@ -1,8 +1,12 @@
 package mx.edu.ebc.comisiones.pojos
 
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.security.core.userdetails.User;
+
 class EbcUser {
 
   String username
   List<Profile> profiles
   def menus
+
 }
