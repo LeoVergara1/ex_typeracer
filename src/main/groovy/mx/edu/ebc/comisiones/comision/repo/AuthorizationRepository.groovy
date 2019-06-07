9838e8247dd23e2feb50cb2b164dce3e9edc0a4d
Brandon Vergara <brandon@makingdevs.com>
2019-06-07 16:09:59 -0500

    coding CP-20   creating respository to authorization commission

---
+++ src/main/groovy/mx/edu/ebc/comisiones/comision/repo/AuthorizationRepository.groovy
@@ -0,0 +1,10 @@
+package mx.edu.ebc.comisiones.comision.repo
+
+import mx.edu.ebc.comisiones.comision.domain.AuthorizationComission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository
+
+@Repository
+public interface AuthorizationRepository extends JpaRepository<AuthorizationComission,Integer> {
+	List<AuthorizationRepository> findAll()
+}
---
+++ src/main/groovy/mx/edu/ebc/comisiones/services/AuthorizationService.groovy
@@ -0,0 +1,9 @@
+package mx.edu.ebc.comisiones.services
+
+import mx.edu.ebc.comisiones.comision.domain.AdminDeComisiones
+import mx.edu.ebc.comisiones.comision.domain.PromoterAssociation
+import mx.edu.ebc.comisiones.pojos.*
+
+public interface AuthorizationService {
+
+}
