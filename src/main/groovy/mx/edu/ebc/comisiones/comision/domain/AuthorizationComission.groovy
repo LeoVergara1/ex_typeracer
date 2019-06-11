aded61b9ee7e99a54e2047070745a8d377aeb9d1
Brandon Vergara <brandon@makingdevs.com>
2019-06-11 16:37:20 -0500

    coding CP-20   rename variables from english to spanish

--- src/main/groovy/mx/edu/ebc/comisiones/comision/domain/AuthorizationComission.groovy
+++ src/main/groovy/mx/edu/ebc/comisiones/comision/domain/AuthorizationComission.groovy
@@ -5,47 +5,73 @@ import javax.persistence.*
 @Entity
 @Table(name = "AUTORIZACION_COMISIONES")
 class AuthorizationComission {
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_ID_AUTOR_COMISIONES")
+  @SequenceGenerator(sequenceName = "SQ_ID_AUTOR_COMISIONES", allocationSize = 1, name = "SQ_ID_AUTOR_COMISIONES")
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Integer id
 	@Column(name = "CAMPUS")
 	String campus
 	@Column(name = "ID_PROMOTOR")
-	String idPromoter
+	String	idPromotor
 	@Column(name = "NOMBRE_PROMOTOR")
-	String promoterName
+	String nombrePromotor
 	@Column(name = "PUESTO")
-	String job
+	String puesto
 	@Column(name = "ID_ALUMNO")
-	String idStudent
+	String idAlumno
 	@Column(name = "NOMBRE_ALUMNO")
-	String studentName
+	String nombreAlumno
 	@Column(name= "PAGO_INICIAL")
-	String initPayment
+	String pagoInicial
 	@Column(name="TOTAL_DESCUENTOS")
-	BigDecimal descountTotal
+	BigDecimal totalDescuentos
 	@Column(name="COMISION")
-	String comission
+	String comision
 	@Column(name="PERIODO")
-	String period
+	String periodo
 	@Column(name="FECHA_DE_PAGO")
-	Date paymenteDate
+	Date fechaDePago
 	@Column(name="AUTORIZADO_DIRECTOR")
-	String directorAuthorize
+	String autorizadoDirector
 	@Column(name="DATE_CREATED")
 	Date dateCreated
 	@Column(name="LAST_UPDATED")
 	Date lastUpdated
 	@Column(name="ID_COORDINADOR")
-	String idCordinater
+	String idCoordinador
 	@Column(name="NOMBRE_COORDINADOR")
-	String cordinaterName
+	String nombreCoordinador
 	@Column(name="COMISION_COORDINADOR")
-	BigDecimal cordinaterComission
+	BigDecimal comisionCoordinador
 	@Column(name="FECHA_AUTORIZADO")
-	Date authorizeDate
+	Date fechaAutorizado
 	@Column(name="USUARIO")
 	String user
+
+	AuthorizationComission(def json, username){
+		this.user = username
+		this.autorizadoDirector = json.autorizadoDirector
+		this.campus = json.campus
+		this.comision = json.comision
+		this.comisionCoordinador = json.comisionCoordinador
+		this.dateCreated = json.dateCreated
+		this.fechaAutorizado = new Date()
+		this.fechaDePago = new Date(Date.parse(json.fechaDePago.replace("-","/")))
+		this.idAlumno = json.idAlumno
+		this.idCoordinador = json.idCoordinador
+		this.idPromotor = json.idPromotor
+		this.nombreAlumno = json.nombreAlumno
+		this.nombreCoordinador = json.nombreCoordinador
+		this.nombrePromotor = json.nombrePromotor
+		this.pagoInicial = json.pagoInicial
+		this.periodo = json.periodo
+		this.puesto = json.puesto
+		this.totalDescuentos = json.totalDescuentos
+		this.dateCreated = new Date()
+		this.lastUpdated = new Date()
+	}
+
+	AuthorizationComission(){}
 
 
 }
