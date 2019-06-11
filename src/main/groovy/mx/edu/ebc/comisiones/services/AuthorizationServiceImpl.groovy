498d354ff1d8fc26293a3366bc7f9df47e8eea6b
Brandon Vergara <brandon@makingdevs.com>
2019-06-11 16:38:13 -0500

    coding CP-20   filter and save authorization already registered

--- src/main/groovy/mx/edu/ebc/comisiones/services/AuthorizationServiceImpl.groovy
+++ src/main/groovy/mx/edu/ebc/comisiones/services/AuthorizationServiceImpl.groovy
@@ -2,16 +2,20 @@ package mx.edu.ebc.comisiones.services
 
 import mx.edu.ebc.comisiones.comision.domain.AdminDeComisiones
 import mx.edu.ebc.comisiones.comision.domain.PromoterAssociation
+import mx.edu.ebc.comisiones.comision.domain.AuthorizationComission
 import mx.edu.ebc.comisiones.pojos.*
 import org.springframework.stereotype.Service
 import mx.edu.ebc.comisiones.comision.storedProcedure.AutorizacionComisionesStoredProcedure
 import org.springframework.beans.factory.annotation.Autowired
+import mx.edu.ebc.comisiones.comision.repo.AuthorizationRepository
 
 @Service
 class AuthorizationServiceImpl implements AuthorizationService {
 
 	@Autowired
 	AutorizacionComisionesStoredProcedure autorizacionComisionesStoredProcedure
+	@Autowired
+	AuthorizationRepository authorizationRepository
 
 	def getCalculation(String campus, String initDate, String finDate){
     Date initDateFrom = new Date(Date.parse(initDate))
@@ -23,6 +27,19 @@ class AuthorizationServiceImpl implements AuthorizationService {
     ]
 		println "*"*100
 		println params
-		autorizacionComisionesStoredProcedure.execute(params)
+		[out_comisiones: filterAuthorizations(params)]
+	}
+
+	def saveListAuthorization(List<AuthorizationComission> listToAuthorization, String username){
+		listToAuthorization.each{ authorized ->
+			authorizationRepository.save(new AuthorizationComission(authorized, username))
+		}
+	}
+
+	def filterAuthorizations(Map params){
+		autorizacionComisionesStoredProcedure.execute(params).out_comisiones.findAll(){
+			//!(authorizationRepository.findByIdPromotorAndIdCoordinadorAndIdAlumno(it.idPromotor, it.idCoordinador, it.idAlumno))
+			true
+		}
 	}
 }
