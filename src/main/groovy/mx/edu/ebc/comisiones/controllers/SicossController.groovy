e38e7374800c4d6ba01517f1681a4a37ada5bd7f
Brandon Vergara <brandon@makingdevs.com>
2019-06-14 16:08:42 -0500

    coding CP-17   I create controller to get sicoss

---
+++ src/main/groovy/mx/edu/ebc/comisiones/controllers/SicossController.groovy
@@ -0,0 +1,50 @@
+package mx.edu.ebc.comisiones.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.servlet.ModelAndView
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import mx.edu.ebc.comisiones.services.AdministrationService
+import mx.edu.ebc.comisiones.services.AuthorizationService
+import mx.edu.ebc.comisiones.services.PromoterService
+import mx.edu.ebc.comisiones.services.SicossService
+import org.springframework.context.ApplicationContext
+import mx.edu.ebc.comisiones.comision.repo.AdminDeComisionesRepository
+import mx.edu.ebc.comisiones.seguridad.repo.CampusRepository
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.servlet.view.RedirectView
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.RequestMethod
+import mx.edu.ebc.comisiones.util.Transform
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.PathVariable
+import mx.edu.ebc.comisiones.comision.domain.Promoter
+import javax.servlet.http.HttpServletRequest;
+
+@RequestMapping("/sicoss")
+@Controller
+class SicossController {
+
+	@Value('#{${campus}}')
+	Map<String, String> campus
+	@Autowired
+	AdministrationService administrationService
+	@Autowired
+	SicossService sicossService
+
+	@GetMapping("/")
+	@ResponseBody
+	ModelAndView sicoss(){
+		ModelAndView model = new ModelAndView("index");
+		model.addObject("content", "sicoss");
+		model
+	}
+
+}
