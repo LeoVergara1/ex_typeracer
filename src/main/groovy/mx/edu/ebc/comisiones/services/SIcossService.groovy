c91dc5f24bc36fa765571ad2052f68a1466f6d14
Brandon Vergara <brandon@makingdevs.com>
2019-06-14 16:08:53 -0500

    coding CP-17   creating files to service to siccoss

---
+++ src/main/groovy/mx/edu/ebc/comisiones/services/SIcossService.groovy
@@ -0,0 +1,6 @@
+package mx.edu.ebc.comisiones.services
+
+import mx.edu.ebc.comisiones.comision.domain.UserCampus
+
+interface SicossService {
+}
---
+++ src/main/groovy/mx/edu/ebc/comisiones/services/SIcossServiceImpl.groovy
@@ -0,0 +1,14 @@
+
+package mx.edu.ebc.comisiones.services
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import wslite.json.JSONObject
+import org.springframework.beans.factory.annotation.Value
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+@Service
+class SicossServiceImpl implements SicossService {
+
+}
