636fb0139e6fa3f6a8ea8ff3ffa7dccccb7ac5ee
Brandon Vergara <brandon@makingdevs.com>
2019-06-18 15:30:16 -0500

    coding CP-29   assing url to menu

--- src/main/groovy/mx/edu/ebc/comisiones/services/PersonServiceImpl.groovy
+++ src/main/groovy/mx/edu/ebc/comisiones/services/PersonServiceImpl.groovy
@@ -162,7 +162,7 @@ Person findPersonByUsername(String username) {
   def getMenusToPerson(String userName){
     def result = restConnectionService.get(clientApiBannerSeguridad, "/v2/api/user/profile/${userName}/Pago%20de%20comisiones")
     println "Init buil"
-    result.accessProfile.collect(){ menu ->
+    result?.accessProfile.collect(){ menu ->
       println menu
       [
         shortName: menu.shortName,
