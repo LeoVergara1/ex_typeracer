5ccfbdc64d0ceb14ab59235a2ee5f7e5382d7d4f
Brandon Vergara <brandon@makingdevs.com>
2019-06-28 16:08:02 -0500

    coding CP-30   adding function to save and get campaign

--- src/main/groovy/mx/edu/ebc/comisiones/services/CampaignService.groovy
+++ src/main/groovy/mx/edu/ebc/comisiones/services/CampaignService.groovy
@@ -1,4 +1,7 @@
 package mx.edu.ebc.comisiones.services
 
+import mx.edu.ebc.comisiones.comision.domain.Campaign
+
 interface CampaignService {
+	List<Campaign> getALlCampaningsByYearAndCreateIfNotExist(String year)
 }
