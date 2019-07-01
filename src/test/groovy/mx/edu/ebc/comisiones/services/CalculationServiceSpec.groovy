f907a15ea4b9271f53ddf93be0ab97445f27e0cc
Brandon Vergara <brandon@makingdevs.com>
2019-07-01 09:09:26 -0500

    coding CP-30   adding services and specs to calculations

---
+++ src/test/groovy/mx/edu/ebc/comisiones/services/CalculationServiceSpec.groovy
@@ -0,0 +1,30 @@
+package mx.edu.ebc.comisiones.services
+
+import org.springframework.stereotype.Repository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
+import mx.edu.ebc.comisiones.comision.repo.*
+import org.springframework.test.context.ContextConfiguration
+import mx.edu.ebc.comisiones.comision.domain.UserCampus
+import spock.lang.Specification
+import spock.lang.Ignore
+import org.springframework.transaction.annotation.Transactional
+
+@SpringBootTest
+@ContextConfiguration
+@TestPropertySource("classpath:application-dev.properties")
+class CalculationServiceSpec extends Specification{
+
+	@Autowired
+  CalculationService calculationService
+
+
+  def "Spect 000 Check service inject"() {
+    when:
+    println calculationService
+    then:
+    assert calculationService
+  }
+
+}
