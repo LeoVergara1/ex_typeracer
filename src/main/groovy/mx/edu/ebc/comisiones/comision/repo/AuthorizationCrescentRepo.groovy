7ed2416841061feac10ec155df0643376cc019ef
Brandon Vergara <brandon@makingdevs.com>
2019-07-01 15:29:51 -0500

    coding CP-30   creating repo to ge crescent

---
+++ src/main/groovy/mx/edu/ebc/comisiones/comision/repo/AuthorizationCrescentRepo.groovy
@@ -0,0 +1,13 @@
+
+import mx.edu.ebc.comisiones.comision.domain.AuthorizationCrescent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository
+
+@Repository
+public interface AuthorizationRepository extends JpaRepository<AuthorizationCrescent,Integer> {
+	List<AuthorizationCrescent> findAll()
+	List<AuthorizationCrescent> findAllByAutorizadoDirector(String autorizadoDirector)
+	List<AuthorizationCrescent> findAllByAutorizadoDirectorAndFechaAutorizadoBetween(String autorizadoDirector, Date initDate, Date finDate)
+	List<AuthorizationCrescent> findAllByAutorizadoDirectorAndCampusAndFechaAutorizadoBetween(String autorizadoDirector, String campus, Date initDate, Date finDate)
+	AuthorizationCrescent findByIdPromotorAndIdCoordinadorAndIdAlumno(String idPromoter, String idCoordinator, String idAlumno)
+}
