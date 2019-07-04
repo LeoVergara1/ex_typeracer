0e075021ed5cd67317fff2e80025f691024e51fd
Brandon Vergara <brandon@makingdevs.com>
2019-07-04 13:42:25 -0500

    coding CP-30  removing class

--- src/main/groovy/mx/edu/ebc/comisiones/WebSecurityConfig.groovy
+++ src/main/groovy/mx/edu/ebc/comisiones/WebSecurityConfig.groovy
@@ -5,16 +5,20 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.boot.autoconfigure.*
-import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler
+import org.springframework.security.web.authentication.logout.LogoutFilter
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
-
-//@Configuration
 //@EnableWebSecurity
-//@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
-public class WebMVCSecurity extends WebSecurityConfigurerAdapter {
-
-  protected void configure(HttpSecurity http) throws Exception {
-	  http.csrf().disable(); // Noncompliant
-	}
-}
+//public class WebSecurityConfig extends
+//WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//            .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+//    }
+//}
