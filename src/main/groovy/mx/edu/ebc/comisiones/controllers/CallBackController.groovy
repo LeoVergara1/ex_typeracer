280cd215b7001b5aa80d78f9b4b478771a8c6f46
Brandon Vergara <brandon@makingdevs.com>
2019-07-04 13:42:50 -0500

    coding CP-30  removing things not used

--- src/main/groovy/mx/edu/ebc/comisiones/controllers/CallBackController.groovy
+++ src/main/groovy/mx/edu/ebc/comisiones/controllers/CallBackController.groovy
@@ -9,31 +9,37 @@ import org.springframework.security.cas.authentication.CasAuthenticationToken
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.ModelAndView
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.servlet.view.RedirectView
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler
 
 @RequestMapping("/cas")
 @Controller
 class CallBackController {
 
-	@Value('${url.apibannerseguridad}')
-	String clientApiBannerSeguridad
-  String roles = "v2/api/user/profile"
-  String profiles = "v2/api/user/role"
+  @Value('${security.cas.server.url.logout}')
+  String casLogout
 
   @RequestMapping("/login")
   String getEbcUserFromCas(Principal principal){
 		def usuarioCas = ((CasAuthenticationToken) principal).getUserDetails()
-    println "*"*100
-    println usuarioCas.dump()
     return "redirect:/"
   }
 
-  @RequestMapping("/")
+  @RequestMapping("/logout")
   @ResponseBody
-  public ModelAndView authorization() {
-		ModelAndView model = new ModelAndView("index");
-		model.addObject("content", "authorization");
-		//def list = adminDeComisionesRepository.findAll()
-		return model
+  RedirectView logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication()
+    if (auth != null){
+        new SecurityContextLogoutHandler().logout(request, response, auth);
+    }
+    RedirectView redirectView = new RedirectView();
+    redirectView.setUrl(casLogout)
+		return redirectView
   }
 
 }
