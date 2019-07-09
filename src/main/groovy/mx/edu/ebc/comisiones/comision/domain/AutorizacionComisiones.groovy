2473a61ee0027b9fb4e8256b6a3cf4170a9302f3
Brandon Vergara <brandon@makingdevs.com>
2019-07-09 11:29:24 -0500

    coding feature/CP-30  adding pidm

--- src/main/groovy/mx/edu/ebc/comisiones/comision/domain/AutorizacionComisiones.groovy
+++ src/main/groovy/mx/edu/ebc/comisiones/comision/domain/AutorizacionComisiones.groovy
@@ -3,28 +3,28 @@ package mx.edu.ebc.comisiones.comision.domain
 import java.util.Date;
 
 class AutorizacionComisiones {
-
-  Long id;
-  String campus;
-  String idPromotor;
-  String nombrePromotor;
-  String puesto;
-  String idAlumno;
-  String nombreAlumno;
-  double pagoInicial;
-  double totalDescuentos;
-  double comision;
-  String periodo;
-  Date fechaDePago;
-  String autorizadoDirector;
-  Date dateCreated;
-  Date lastUpdated;
-  String idCoordinador;
-  String nombreCoordinador;
-  double comisionCoordinador;
-  Date fechaAutorizado;
-  String usuario;
-  String tipoPago;
-  double valorContratoReal;
+  Long id
+  String campus
+  String idPromotor
+  String nombrePromotor
+  String puesto
+  String idAlumno
+  String nombreAlumno
+  double pagoInicial
+  double totalDescuentos
+  double comision
+  String periodo
+  Date fechaDePago
+  String autorizadoDirector
+  Date dateCreated
+  Date lastUpdated
+  String idCoordinador
+  String nombreCoordinador
+  double comisionCoordinador
+  Date fechaAutorizado
+  String usuario
+  String tipoPago
+  double valorContratoReal
+  Long pidm
 
 	}
