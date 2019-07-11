3a346edf6745ee120ac9e8568c31fff14421a010
Brandon Vergara <brandon@makingdevs.com>
2019-07-11 15:27:50 -0500

    coding   finishig test to get authorizations crecent

--- src/test/groovy/mx/edu/ebc/comisiones/services/CalculationServiceSpec.groovy
+++ src/test/groovy/mx/edu/ebc/comisiones/services/CalculationServiceSpec.groovy
@@ -9,6 +9,8 @@ import org.springframework.test.context.ContextConfiguration
 import mx.edu.ebc.comisiones.comision.domain.UserCampus
 import mx.edu.ebc.comisiones.comision.domain.Campaign
 import mx.edu.ebc.comisiones.comision.domain.Trimester
+import mx.edu.ebc.comisiones.comision.domain.Goal
+import mx.edu.ebc.comisiones.comision.domain.AuthorizationCrescent
 import spock.lang.Specification
 import spock.lang.Ignore
 import spock.lang.Unroll
@@ -110,15 +112,53 @@ class CalculationServiceSpec extends Specification{
 	@Unroll
 	def "2 obtener la campaña correspondiente al trimestre"(){
 		given: "Trimestre"
-			Trimester trimester = new Trimester(clave: "20ANYNAME", year: "2019")
+			Trimester trimester = new Trimester(clave: _clave, year: _year)
 		when: "Se obtiene la Campaña"
 			Campaign campaign = calculationService.getCampaignByTrimester(trimester)
 		then:
 			campaign?.class == _result
 		where:
-		_name 			| _year || _result
+		_clave			| _year || _result
 		"20ANYNAME" | "2019"|| Campaign
-		"40OTHER"		| "2019"|| null
+		"40OTHER"		| "2019"|| Campaign
+		"30OTHER"		| "2019"|| null
+	}
+
+	@Unroll
+	def "3 Obtener todas las autorizaciones validas para esa campaña por el campus"(){
+		given:"una campaña"
+			Trimester trimester = new Trimester(clave: "20ANYNAME", year: "2019")
+			Campaign campaign = calculationService.getCampaignByTrimester(trimester)
+		and: "Meta por campus"
+			Goal goal = new Goal(numRegisters: _numRegisters, percentCommission: 10, campus: "CMX")
+		when: "Se obtienen autorizaciones para crecientes con calculos"
+			List<AuthorizationCrescent> authorizationCrescents = calculationService.calculationByGoal(goal, campaign.initDate, campaign.endDate)
+		then: "Si cumple habra elementos"
+			authorizationCrescents.size() >= _result
+		where:
+		_numRegisters	|| _result
+		1 						|| 1
+		10						|| 0
+	}
+
+	def "4 Calcular la comision del coordinador"(){
+		given: "Valor de contrato y porcentage de comision"
+			double valorContratoReal = 62000
+			float percentCommission = 15
+		when: "Se calcula la comision"
+			double comissionCoordinater = calculationService.calculationComissionCoordinater(valorContratoReal, percentCommission)
+		then:
+			comissionCoordinater == 50
+	}
+
+	def "5 Calcular la comision del promotor"(){
+		given: "Valor de contrato y porcentage de comision"
+			double valorContratoReal = 62000
+			float percentCommission = 15
+		when: "Se calcula la comision"
+			double comissionPromoter = calculationService.calculationComissionPromoter(valorContratoReal, percentCommission)
+		then:
+			comissionPromoter == 50
 	}
 
 }
