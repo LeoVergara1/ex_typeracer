7884193afe3370ea35144ce7d43cd375a2732438
Brandon Vergara <brandon@makingdevs.com>
2019-07-11 14:57:41 -0500

    coding   creating test to get campaing from trimestre

--- src/test/groovy/mx/edu/ebc/comisiones/services/CalculationServiceSpec.groovy
+++ src/test/groovy/mx/edu/ebc/comisiones/services/CalculationServiceSpec.groovy
@@ -11,6 +11,7 @@ import mx.edu.ebc.comisiones.comision.domain.Campaign
 import mx.edu.ebc.comisiones.comision.domain.Trimester
 import spock.lang.Specification
 import spock.lang.Ignore
+import spock.lang.Unroll
 import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
@@ -26,6 +27,8 @@ class CalculationServiceSpec extends Specification{
 	GoalRepository goalRepository
   @Autowired
   TrimesterRepository trimesterRepository
+	@Autowired
+	TrimesterService trimesterService
 
 
   def "Spect 000 Check service inject"() {
@@ -51,7 +54,7 @@ class CalculationServiceSpec extends Specification{
 		when: ""
 			def result = calculationService.calculationByGoal(goal, new Date(Date.parse("2019/1/1")), new Date(Date.parse("2019/7/1")))
 		then: ""
-			goal	
+			goal
 	}
 
 	def "Check authorization in banner to current status of authorization"(){
@@ -60,7 +63,7 @@ class CalculationServiceSpec extends Specification{
 			println goal.dump()
 		and: "Get trimester by goal"
 			Campaign campaign = calculationService.getCampaignByTrimester(goal.trimester)
-		and: "Authorizations to update"	
+		and: "Authorizations to update"
 			def authorizationsCommissions = calculationService.calculationByGoal(goal, campaign.initDate, campaign.endDate)
 			println "Calculados"
 			println authorizationsCommissions
@@ -87,6 +90,35 @@ class CalculationServiceSpec extends Specification{
 		then:
 		campaign
 
+	}
+
+	@Unroll
+	def "1 Buscar trimestre para el calculo"(){
+		given: "Rango de fechas para la busqueda"
+			String initDate = _initDate
+			String endDate = _endDate
+		when: "Se obtine el trimeste"
+			List<Trimester> trimesters = trimesterService.findByInitDateGreaterThanAndEndDateLessThan(initDate, endDate)
+		then: ""
+			trimesters.size() == _result
+    where:
+    _initDate   | _endDate 		 || _result
+    "01/01/2019"| "11/07/2019" ||  1
+    "01/01/2019"| "11/11/2019" ||  2
+	}
+
+	@Unroll
+	def "2 obtener la campaña correspondiente al trimestre"(){
+		given: "Trimestre"
+			Trimester trimester = new Trimester(clave: "20ANYNAME", year: "2019")
+		when: "Se obtiene la Campaña"
+			Campaign campaign = calculationService.getCampaignByTrimester(trimester)
+		then:
+			campaign?.class == _result
+		where:
+		_name 			| _year || _result
+		"20ANYNAME" | "2019"|| Campaign
+		"40OTHER"		| "2019"|| null
 	}
 
 }
