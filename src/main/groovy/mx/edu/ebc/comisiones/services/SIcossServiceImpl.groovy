74f3c77fffb5630a3d146f39b2e0b91033e63fe3
Brandon Vergara <brandon@makingdevs.com>
2019-07-23 13:51:05 -0500

    coding CP-22  finish test to duplicates coordinates and promoters

--- src/main/groovy/mx/edu/ebc/comisiones/services/SIcossServiceImpl.groovy
+++ src/main/groovy/mx/edu/ebc/comisiones/services/SIcossServiceImpl.groovy
@@ -33,12 +33,33 @@ class SicossServiceImpl implements SicossService {
   }
 
   List<Sicoss> separetePromoterAndCoordinater(def listAuthorizationComission){
+    Date currentDate = new Date()
     List<Sicoss> listSicoss = []
     listAuthorizationComission.each(){ commission ->
       if(commission.idCoordinador){
-        listSicoss << new Sicoss(claveEmployee: commission.adCoordinador.replace("AD", ""))
+        listSicoss << new Sicoss(claveEmployee: commission.adCoordinador.replace("AD", ""), dateMovenment: new Date("${currentDate.month+1}/01/${currentDate.year+1900}"))
       }
-      listSicoss << new Sicoss(claveEmployee: commission.adPromotor.replace("AD", ""))
+      listSicoss << new Sicoss(claveEmployee: commission.adPromotor.replace("AD", ""),  dateMovenment: new Date("${currentDate.month+1}/01/${currentDate.year+1900}"))
     }
+    listSicoss
+  }
+
+  List<Sicoss> plusOneDayThanMoreClavesSames(List<Sicoss> listSicoss){
+    Date currentDate = new Date()
+    List<Sicoss> listSicossWithoutDuplicates = []
+    def groups = listSicoss.groupBy({ it.claveEmployee })
+    groups.each(){ key, value ->
+      if(value.size() > 1){
+        value.eachWithIndex(){ element, index ->
+          int day = 1 + index
+          element.dateMovenment = new Date("${currentDate.month+1}/${day}/${currentDate.year+1900}") 
+          listSicossWithoutDuplicates << element
+        }
+      }
+      else{
+        listSicossWithoutDuplicates << value[0]
+      }
+    }
+    listSicoss
   }
 }
