1c5e40e7f236eee5684563cd68101077f294e162
Brandon Vergara <brandon@makingdevs.com>
2019-07-24 16:29:31 -0500

    coding CP-22  creating repository to save sicoss

---
+++ src/main/groovy/mx/edu/ebc/comisiones/comision/repo/SicossRepository.groovy
@@ -0,0 +1,10 @@
+package mx.edu.ebc.comisiones.comision.repo
+
+import mx.edu.ebc.comisiones.comision.domain.Sicoss;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository
+import org.springframework.data.jpa.repository.Query
+
+@Repository
+interface SicossRepository extends JpaRepository<Sicoss, Integer> {
+}
