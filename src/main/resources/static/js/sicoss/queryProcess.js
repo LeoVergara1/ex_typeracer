e599f518ed6af1ca0a4d8427701cd37c8d663e82
Brandon Vergara <brandon@makingdevs.com>
2019-07-26 16:09:34 -0500

    coding CP-22  adding snotify to view

--- src/main/resources/static/js/sicoss/queryProcess.js
+++ src/main/resources/static/js/sicoss/queryProcess.js
@@ -13,6 +13,14 @@ var app = new Vue({
       months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
       weekDays: ["Lun", "Mar", "Mier", "Jue", "Vie", "Sab", "Dom"]
     },
+    notifyOptions: {
+      timeout: 9000,
+      showProgressBar: true,
+      closeOnClick: false,
+      pauseOnHover: true,
+      position: 'rightTop',
+      helperNotificationCycle: true
+    },
     loader:{
       color: '#0b93d1',
       height: '15px',
@@ -22,6 +30,8 @@ var app = new Vue({
       loading: false,
       size: "95px",
     },
+    listSicoss: [],
+    waitingExcel: false,
   },
   computed: {
 
@@ -42,16 +52,61 @@ var app = new Vue({
   },
   methods:{
     getCommissionsToEmployee(){
-      this.$http.get(`/sicoss/employee/${this.query.clave}/`, {params: {foo: 'bar'}}).then(response => {
+      this.$http.get(`/sicoss/employee/${this.query.clave}/`, {params: {initDate: this.query.selectInit, endDate: this.query.selectFin}}).then(response => {
         console.log(response.body);
-        this.campuses = response.body.campus
-        this.person = response.body.person.person
         this.loader.loading = false
+        this.listSicoss = response.body
+        if(this.listSicoss.length == 0){
+          this.$snotify.warning("No se encontro infromación", this.notifyOptions)
+        }
+        else{
+          this.$bvModal.show('modal-download')
+        }
       }, response => {
         console.log("Fail")
         console.log(response)
       }) 
-    }
+    },
+    generateExcel(){
+      this.waitingExcel = true
+			console.log("Inicia Construcción Excel")
+      const ws_data = [
+        ["CLAVE EMPLEADO", "TIPO DE NÓMINA", "CLAVE NÓMINA",	"CONCEPTO",	"FECHA DE MOVIMIENTO",	"DEPARTAMENTO",	"REFERENCA 2",	"DATO",	"IMPORTE",	"SALDO",	"NÓMINA ABIERTA"],
+			];
+			var wb = XLSX.utils.book_new()
+			const workSheet = XLSX.utils.json_to_sheet(this.builtDataToExcel(this.listSicoss), {
+        origin: "A2",
+        Header: ['Column 1', 'Column 2', 'Column 3'],
+        SkpHeader: true // skip the title line above
+			})
+			XLSX.utils.sheet_add_aoa(workSheet, ws_data, {
+        Origin: 'A1'// Add content from A1
+      });
+      XLSX.utils.book_append_sheet(wb, workSheet, 'comisiones')
+      XLSX.writeFile(wb, 'sicoss.xlsx')
+      this.waitingExcel = false
+      console.log("Termina Construcción Excel")
+    },
+    builtDataToExcel(array){
+      let dataToExcel = []
+      array.forEach(element => {
+       json = {
+        "TR_NUMERO": element.claveEmployee ? element.claveEmployee : 0,
+        "NOMI_TIPONOMINA": element.typePaysheet,
+        "NOMI_CLAVE": element.clavePaysheet,
+        "NOMI_CONCEPTO": element.concept,
+        "NOMI_FECHA": element.dateMovenment,
+        "NOMI_REFERENCIA": element.reference1,
+        "NOMI_REFERENCIA2": element.reference2,
+        "NOMI_DATO": element.dataPayhseet,
+        "NOMI_IMPORTE": element.importe,
+				"NOMI_SALDO": element.salary,
+				"NOMINA_ABIERTA": element.typePaysheet
+       }
+      dataToExcel.push(json)
+      });
+      return dataToExcel
+		}
   },
   mounted() {
     this.$root.$on('bv::modal::hide', (bvEvent, modalId) => {
