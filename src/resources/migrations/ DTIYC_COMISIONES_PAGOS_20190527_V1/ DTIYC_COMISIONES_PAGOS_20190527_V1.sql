db919459654d65c10986657c1527f8561cfe31cd
Brandon Vergara <brandon@makingdevs.com>
2019-08-07 11:30:02 -0500

    coding CP-46  adding require to QA

---
+++ src/resources/migrations/ DTIYC_COMISIONES_PAGOS_20190527_V1/ DTIYC_COMISIONES_PAGOS_20190527_V1.sql
@@ -0,0 +1,32 @@
+REM ***************************************************************************
+PROMPT Nombre del Script: DTIYC_COMISIONES_PAGOS_20190527_V1
+PROMPT Proyecto: CMISIONES_PAGOS
+PROMPT VMR 27 de Junio de 2018
+PROMPT Se modifica esquema para la aplicacion 
+REM ***************************************************************************
+
+
+SET ECHO OFF
+SET VERIFY OFF
+
+SPOOL DTIYC_COMISIONES_PAGOS_20190527_V1.log
+
+PROMPT ***********************************************************************
+ACCEPT BANINST1_PASSWORD PROMPT 'BANINST1 Password:' HIDE
+CONNECT baninst1/&&baninst1_password
+PROMPT ***********************************************************************
+
+SELECT 'Fecha de ejecución: ' || TO_CHAR(SYSDATE,'DD-MON-YYYY HH24:MI') Fecha_Ejecucion FROM DUAL;
+
+SET DEFINE OFF
+
+PROMPT * EJECUCION DE SCRIPT *
+@ca_user_campus.sql
+
+SHOW ERRORS
+
+PROMPT Close output file.
+
+SPOOL OFF
+
+EXIT
