1b18133b9dbaa0e7abeef38249bae97a3e9bba78
Jorge Acosta Lemus <jorge@makingdevs.com>
2019-08-20 10:52:05 -0500

    coding CP-48 Adding url for acas prod

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ docker rm $PROJECT_NAME
 
 docker pull 172.31.100.23:5000/ebc/springboot/$PROJECT_NAME
 
-docker run -dit --restart unless-stopped --network general-ebc-network --add-host apr-test.ebc.edu.mx:10.31.23.14 --add-host apr.ebc.edu.mx: --name $PROJECT_NAME -d -p $PORT:8090 172.31.100.23:5000/ebc/springboot/$PROJECT_NAME
+docker run -dit --restart unless-stopped --network general-ebc-network --add-host apr-test.ebc.edu.mx:10.31.23.14 --add-host apr.ebc.edu.mx:10.31.22.24 --name $PROJECT_NAME -d -p $PORT:8090 172.31.100.23:5000/ebc/springboot/$PROJECT_NAME
