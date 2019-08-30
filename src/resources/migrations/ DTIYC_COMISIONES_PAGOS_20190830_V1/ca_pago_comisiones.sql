f68e7f566d496785d12e38e1e72807de3c657f7a
Brandon Vergara <brandon@makingdevs.com>
2019-08-30 14:12:06 -0500

    coding CP-46  dding script to add column

---
+++ src/resources/migrations/ DTIYC_COMISIONES_PAGOS_20190830_V1/ca_pago_comisiones.sql
@@ -0,0 +1,11 @@
+/*
+REM **************************************************************************
+REM Nombre del Script: ca_pago_comisiones.sql
+REM Proyecto: api-comisiones
+REM --------------------------------------------------------------------------
+REM AUDIT TRAIL:  Version 1
+REM Realizado por: VMRB, 30  de Julio  de 2019
+*/
+
+ALTER TABLE PGOCOMIS.ASOCIACION_PROMOTOR ADD ID NUMBER;
+commit;
