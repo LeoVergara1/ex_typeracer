0119de42bcd335722c7c7046628d5858f9e0ad2d
Brandon Vergara <brandon@makingdevs.com>
2019-10-16 13:13:54 -0500

    coding CP-52  adding implemetantions to save authorizations with status marketing

--- src/main/groovy/mx/edu/ebc/comisiones/services/AuthorizationServiceImpl.groovy
+++ src/main/groovy/mx/edu/ebc/comisiones/services/AuthorizationServiceImpl.groovy
@@ -43,15 +43,38 @@ class AuthorizationServiceImpl implements AuthorizationService {
 		[out_comisiones: comissions, comissionsGroups: comissions.groupBy({ it.idPromotor })]
 	}
 
+	def getCalculationMarketing(String campus, String initDate, String finDate){
+    Date initDateFrom = new SimpleDateFormat("dd/MM/yyyy").parse(initDate)
+    Date finDateFrom = new SimpleDateFormat("dd/MM/yyyy").parse(finDate)
+		def comissions = authorizationRepository.findAllByStatusMarketingAndCampusAndFechaAutorizadoBetween(false, campus, initDateFrom, finDateFrom)
+		[out_comisiones: comissions, comissionsGroups: comissions.groupBy({ it.idPromotor })]
+	}
+
 	def saveListAuthorization(List<AuthorizationComission> listToAuthorization, String username){
 		listToAuthorization.each{ authorized ->
 			authorizationRepository.save(new AuthorizationComission(authorized, username))
 		}
 	}
 
+	def saveListAuthorizationMarketing(List<AuthorizationComission> listToAuthorization, String username){
+		println "+"*100
+		println listToAuthorization.dump() 
+		listToAuthorization.each{ authorized ->
+			authorized.usernameMarketing = username
+			authorizationRepository.save(authorized)
+		}
+	}
+
 	def saveListAuthorizationCrecent(List<AuthorizationCrescent> listToAuthorization, String username){
 		listToAuthorization.each{ authorized ->
 			authorized.user = username
+			authorizationRepository.save(authorized)
+		}
+	}
+
+	def saveListAuthorizationCrecentMarketing(List<AuthorizationCrescent> listToAuthorization, String username){
+		listToAuthorization.each{ authorized ->
+			authorized.usernameMarketing = username
 			authorizationRepository.save(authorized)
 		}
 	}
