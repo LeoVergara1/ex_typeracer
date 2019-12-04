aa2b3cae0a2c17185b133392ca1e0ed73a5182d5
Brandon Vergara <brandon@makingdevs.com>
2019-12-04 16:05:53 -0600

    coding CP-56  adding edit and delete with table render

--- src/main/resources/static/js/register/promoterComponent.js
+++ src/main/resources/static/js/register/promoterComponent.js
@@ -1,19 +1,24 @@
 Vue.component('template-promoter', {
 	props: {
-			campus: Object
 	},
   data: function () {
     return {
 			count: 0,
-			listAssociation: []
+			listAssociation: [],
+			roles: {},
+			campus: {},
+			userModal: {
+				userCampusPK: {}
+			}
     }
 	},
 	created: function (){
 		console.log("Instancia creada de componente")
-		this.$http.get('/administration/data/association').then(response => {
+		this.$http.get('/administration/data/registers').then(response => {
 			this.someData = response.body;
 			console.log(response.body);
-			this.campus.list = response.body.campus
+			this.campus = response.body.campus 
+			this.roles = response.body.roles
 			this.listAssociation = response.body.listAssociation
 			console.log(response)
 			console.log("regreso")
@@ -25,40 +30,129 @@ Vue.component('template-promoter', {
 	methods: {
 		getAssociation: function(){
 
-		}
+		},
+		setUserToEdit(register){
+			this.userModal = register
+			this.$bvModal.show('modal-edit')
+		},
+		getDescriptionToDivision: function (value){
+			return this.campus[value] ? this.campus[value] : value
+		},
+	},
+	filters: {
+		getDescriptionToRol: function (value){
+			let map = {
+				"DIR_CAMPUS": "Director de campus",
+				"ADMIN_JP": "Administrador",
+				"COORD_MERCADOTECNIA_CORP": "Coordinador",
+				"NOMINA_ADMIN_SICOSS": "Sicoss de nomina",
+				"JEFE_PROMOCION": "Jefe de promoción",
+				"Promotor": "Promotor",
+				"PROMOCIÓN": "Promoción"
+			}
+			return map[value] ? map[value] : value
+		},
 	},
 	template: `
-	<div class="row">
+	<div>
+			<b-modal id="modal-edit" title="Eiditar usuario">
+				<div class="row">
+					<div class="col">
+						<h4>Username: {{userModal.userCampusPK.userName }}</h4>
+					</div>
+				</div>
+				<div class="row">
+						<div class="col-lg-6">
+								<label for="selectCampus" class="font-weight-bold">Campus</label>
+								<div id="filter-campus"><select class="form-control">
+										<option value=""> Todas </option>
+										<option v-for="(k, v) in campus" v-bind:value="v">
+											{{ k }}
+										</option>
+									</select>
+								</div>
+							</div>
+							<div class="col-lg-6"><label for="selectRoles" class="font-weight-bold">Rol</label>
+								<div id="filter-roles"><select class="form-control filtersRolAndCampus">
+										<option value="0"> Todas </option>
+										<option v-for="rol in roles" v-bind:value="rol.nidRol">
+												{{ rol.descriptionRol | getDescriptionToRol }}
+											</option>
+									</select>
+								</div>
+							</div>
+				</div>
+				<template slot="modal-footer" class="w-100">
+							<b-button variant="primary" size="sm" class="float-right" @click="$bvModal.hide('modal-edit')">
+									Cerrar
+								</b-button>
+					</template>
+			</b-modal>
+		<div class="row">
 			<div class="col-lg-12">
-					<table class="table table-striped">
-							<thead>
-								<tr>
-									<th scope="col">Nombre Promotor</th>
-									<th scope="col">ID Promotor</th>
-									<th scope="col">Puesto</th>
-									<th scope="col">Clave de empleado</th>
-									<th scope="col">Campus</th>
-									<th scope="col">ID Coordinador</th>
-									<th scope="col">Nombre Coordinador</th>
-									<th scope="col">Clave de empleado</th>
-									<th scope="col">Habilitado</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr v-for="association in listAssociation" v-if="association.hasOwnProperty('idPromoter')">
-									<th scope="row">{{association.promoterName}}</th>
-									<td>{{association.idPromoter}}</td>
-									<td>{{association.jobPromoter}}</td>
-									<td>{{association.clavePromoter}}</td>
-									<td>{{association.campusDesc}}</td>
-									<td>{{association.idCoordinater}}</td>
-									<td>{{association.coordinaterName}}</td>
-									<td>{{association.claveCoordinater}}</td>
-									<td>{{association.relationActive}}</td>
-								</tr>
-							</tbody>
-						</table>
+				<h3 class="blue margin0">Búsqueda
+				</h3>
 			</div>
+		</div>
+		<div class="row">
+			<div id="recrCodeDiv" class="col-lg-2">
+				<label for="recrCodeInput" class="font-weight-bold">Usuario</label>
+				<input type="text" id="usernameId" placeholder="Usuario" class="form-control">
+			</div>
+			<div class="col-lg-2">
+				<label for="selectCampus" class="font-weight-bold">Campus</label>
+				<div id="filter-campus"><select class="form-control">
+						<option value=""> Todas </option>
+						<option v-for="(k, v) in campus" v-bind:value="v">
+							{{ k }}
+						</option>
+					</select>
+				</div>
+			</div>
+			<div class="col-lg-2"><label for="selectRoles" class="font-weight-bold">Rol</label>
+				<div id="filter-roles"><select class="form-control filtersRolAndCampus">
+						<option value="0"> Todas </option>
+						<option v-for="rol in roles" v-bind:value="rol.nidRol">
+								{{ rol.descriptionRol | getDescriptionToRol }}
+							</option>
+					</select>
+				</div>
+			</div>
+			<div class="col-auto"><button class="btn btn-primary " style="margin-top: 32px;"><i aria-hidden="true"
+						class="fa fa-search"></i>
+					Buscar</button>
+				</div>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="col-lg-12">
+				<table class="table table-striped">
+					<thead>
+						<tr>
+							<th scope="col">Nombre</th>
+							<th scope="col">Usuario</th>
+							<th scope="col">Campus</th>
+							<th scope="col">Rol</th>
+							<th scope="col"></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="register in listAssociation">
+							<th scope="row">{{register.nameLong}}</th>
+							<td>{{register.userCampusPK.userName}}</td>
+							<td>{{getDescriptionToDivision(register.userCampusPK.campusCode)}}</td>
+							<td>{{register.roleDescription}}</td>
+							<th>
+									<div class="btn-group" role="group" aria-label="Basic example">
+											<button class="btn btn-success btn-xs" @click="setUserToEdit(register)"><i class="fa fa-minus" aria-hidden="true"></i> Editar</button>
+											<button class="btn btn-danger btn-xs" ><i class="fa fa-plus" aria-hidden="true"></i> Eliminar</button>
+									</div>
+							</th>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
 	</div>
 	`
 })
